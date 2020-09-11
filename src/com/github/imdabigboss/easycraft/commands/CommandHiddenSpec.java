25098a45594cbb52b3fcd1746baba75cf607d3fc
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2020-09-11 18:32:31 +0200

    Changes, see releases

---
+++ src/com/github/imdabigboss/easycraft/commands/CommandHiddenSpec.java
@@ -0,0 +1,49 @@
+package com.github.imdabigboss.easycraft.commands;
+
+import org.bukkit.GameMode;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import com.github.imdabigboss.easycraft.easyCraft;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class CommandHiddenSpec implements CommandExecutor {
+	private Plugin plugin = easyCraft.getPlugin();
+
+    // This method is called, when somebody uses our command
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    	if (sender instanceof Player) { //Get if executing origin is a player
+    	} else {
+    		sender.sendMessage(ChatColor.RED + "You must be a player to run this command!");
+    		return true;
+    	}
+    	
+    	if (args.length != 1) {
+    		sendHelp(sender);
+    		return true;
+    	}
+    	
+    	Player player = (Player) sender;
+    	
+    	if (args[0].equalsIgnoreCase("on")) {
+    		player.setGameMode(GameMode.SPECTATOR);
+    		player.setPlayerListName(ChatColor.RESET + player.getDisplayName());
+    	} else if (args[0].equalsIgnoreCase("off")) {
+    		player.setGameMode(GameMode.SURVIVAL);
+    		player.setPlayerListName(ChatColor.RESET + player.getDisplayName());
+    	} else {
+    		sendHelp(sender);
+    	}
+    		
+    	return true;
+    }
+    
+    private void sendHelp(CommandSender sender) {
+    	sender.sendMessage("The correct usage is:");
+    	sender.sendMessage("- /vanish on");
+    	sender.sendMessage("- /vanish off");
+    }
+}
